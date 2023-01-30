45ce55d19876844520c078a85c22acf7e748da44
Clécio Sousa <54221604+cleciosousa@users.noreply.github.com>
2023-01-30 20:05:35 -0300

    Update javascrript.js

--- javaScript/javascrript.js
+++ javaScript/javascrript.js
@@ -1,6 +1,15 @@
 let menuIcon = document.querySelector('#icon-btn')
 let menuMobile = document.querySelector('.hidden')
 
+let like1 = document.querySelector('#logo-like1')
+let like2 = document.querySelector('#logo-like2')
+let like3 = document.querySelector('#logo-like3')
+let numberLike1 = document.querySelector('#number-like1')
+let numberLike2 = document.querySelector('#number-like2')
+let numberLike3 = document.querySelector('#number-like3')
+
+
+
 menuIcon.addEventListener('click',()=>{
     if(menuMobile.style.display == 'block'){
         menuMobile.style.display = 'none'
@@ -8,3 +17,16 @@ menuIcon.addEventListener('click',()=>{
         menuMobile.style.display = 'block'
     }
 })
+
+like1.addEventListener('click',()=>{
+    numberLike1.innerHTML = `${Number(numberLike1.textContent) +1}`
+})
+
+like2.addEventListener('click',()=>{
+    numberLike2.innerHTML = `${Number(numberLike2.textContent) +1}`
+})
+
+like3.addEventListener('click',()=>{
+    numberLike3.innerHTML = `${Number(numberLike3.textContent) +1}`
+})
+
